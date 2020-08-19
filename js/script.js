6274836a945d6033bded57c8dbae8fb3dec59c17
Zub Aleksandr <dynamics1995@gmail.com>
2020-08-20 02:02:34 +0300

    test foreach

---
+++ js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const test = document.querySelectorAll("input");
+		let mass = [test];
+		console.log(test);
+		mass.forEach(function () {
+			const numA1 = parseInt(
+				form.querySelector('input[name="a1"]').value
+			);
+			const numA2 = parseInt(
+				form.querySelector('input[name="a2"]').value
+			);
+			const numA3 = parseInt(
+				form.querySelector('input[name="a3"]').value
+			);
+			console.log(mass);
+			
+	});
+});
+function mac() {
+	alert(`${numA1} ${numA2} ${numA3}`);
+}
