13529b66cd281326ba0c7d58cf888c0c4a064ba1
Vladimir Kornienko <36392011+VladKorn@users.noreply.github.com>
2020-08-20 20:26:24 +0300

    ....

--- js/script.js
+++ js/script.js
@@ -2,16 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	const test = document.querySelectorAll("input");
 	
 		console.log(test);
-		test.forEach(function () {
-			const numA1 = parseInt(
-				form.querySelector('input[name="a1"]').value
-			);
-			const numA2 = parseInt(
-				form.querySelector('input[name="a2"]').value
-			);
-			const numA3 = parseInt(
-				form.querySelector('input[name="a3"]').value
-			);
+		test.forEach( (inputEl)=> {
+			inputEl.addEvent...
 			console.log(mass);
 			
 	});
