c7dece9eb28ac227c002597ee0ccbe5b6fa2ae48
Zub Aleksandr <dynamics1995@gmail.com>
2020-08-29 08:40:49 +0300

    work

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const test = document.querySelectorAll("input");
-		let mass = [test];
-		console.log(test);
-		mass.forEach(function () {
-			const numA1 = parseInt(
-				form.querySelector('input[name="a1"]').value
-			);
-			const numA2 = parseInt(
-				form.querySelector('input[name="a2"]').value
-			);
-			const numA3 = parseInt(
-				form.querySelector('input[name="a3"]').value
-			);
-			console.log(mass);
-			
-	});
-});
+let colum = document.querySelectorAll("input");
+colum.forEach((inputEl)=>{
+	inputEl.addEventListener("change", mac())
+})
 function mac() {
-	alert(`${numA1} ${numA2} ${numA3}`);
+	alert("input");
 }
