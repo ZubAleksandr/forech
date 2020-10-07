217a7d77c115745810738d3cde96c1871ee8525a
Vladimir Kornienko <36392011+VladKorn@users.noreply.github.com>
2020-10-08 02:03:06 +0300

    explane

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ let cof = [1.5, 1.5, 1.5, 1, 1.4, 1, 131, 141, 54, 45, 54, 66];
 let findMoreThanOne = (arr) => {
 	let itog = [];
 	arr.forEach((value) => {
-		if (arr.filter((el) => el === value).length > 1) {
+		console.log("value" , value)
+		const arrayOfSameValues = arr.filter((el) => el === value);
+		console.log("arrayOfSameValues" , arrayOfSameValues)
+		if (arrayOfSameValues.length > 1) {
 			itog.push(value);
 		}
 	});
