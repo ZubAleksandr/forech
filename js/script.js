145f62c09d6cd6d654806082c44f48007ee3a841
Vladimir Kornienko <36392011+VladKorn@users.noreply.github.com>
2020-10-08 01:50:42 +0300

    forEach

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-  
-let km = [1, 0.25, 0.5, 0.75, 1, 0.25]
+let km = [1, 0.25, 0.5, 0.75, 1, 0.25];
 
-let cof =[1.5, 1.5, 1.5, 1, 1.4, 1 , 131, 141 ,54 ,45 ,54 ,66];
+let cof = [1.5, 1.5, 1.5, 1, 1.4, 1, 131, 141, 54, 45, 54, 66];
 
 let findMoreThanOne = (arr) => {
 	let itog = [];
-	for (let i = 0; i < arr.length; i++) {
-		if(arr.filter(el=>el === arr[i]).length > 1){
+	arr.forEach((value) => {
+		if (arr.filter((el) => el === value).length > 1) {
 			itog.push(cof[i]);
 		}
-	}
+	});
 
 	return itog;
 };
 
 const res = findMoreThanOne(cof);
-console.log("end" , res);
-	
+console.log("end", res);
